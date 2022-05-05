525227ef584f93801b1dfa58e14131262a98799a
LuckyShmucky <christianluciano567@gmail.com>
2022-05-04 19:55:57 -0700

    features/ sequelize is set up and working on band model

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
-const { Sequelize } = require('sequelize')
+const  {Sequelize}  = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
@@ -10,26 +10,42 @@ app.use(express.urlencoded({ extended: false }))
 
 // SEQUELIZE CONNECTION
 // const sequelize = new Sequelize(process.env.PG_URI)
-const sequelize = new Sequelize(process.env.PG_URI, 'postgres', process.env.PG_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres'
-  })
-  
-    try{
-        sequelize.authenticate()
-        console.log(`Connected with sequelize ${process.env.PG_URI}`)
-        sequelize.close()
-    } catch(err){
-        console.log(`Unable to connect to PG: ${err}`)
-    }
-
-
+// const sequelize = new Sequelize(process.env.PG_URI, 'postgres', process.env.PG_PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'postgres'
+//   })
 // ROOT
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to the Tour API'
-    })
-})
+
+//Mike's solution
+let sequelize = new Sequelize({ 
+    username: "postgres",
+    password: "Wishingwell999!",
+    host: "127.0.0.1",
+    dialect: "postgres"})
+    
+    
+    //Amanda's solution
+    // const sequelize = new Sequelize(process.env.PG_URI, 'postgres', process.env.PG_PASSWORD, {
+        //     username: 'postgres',
+        //     host: 'localhost',
+        //     dialect: 'postgres'
+        // });
+        
+        
+        
+        try{
+            sequelize.authenticate()
+            console.log(`Connected with sequelize ${process.env.PG_URI}`)
+        } catch(err){
+            console.log(`Unable to connect to PG: ${err}`)
+        }
+        
+        app.get('/', (req, res) => {
+            res.status(200).json({
+                message: 'Welcome to the Tour API'
+            })
+        })
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
